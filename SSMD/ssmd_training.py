29e5abbdb607d29a967a1a1cbc9f89998b8c1e9d
wsteams <53390550+wsteams@users.noreply.github.com>
2021-06-19 23:38:10 +0900

    Update ssmd_training.py

--- SSMD/ssmd_training.py
+++ SSMD/ssmd_training.py
@@ -133,7 +133,7 @@ def convolution(weights, pad=True, stride=1, name=''):
 def batch_normalization(scale, bias, mean, variance, spatial=True, name=''):
     scale = C.Constant(value=scale, name='scale')
     bias = C.Constant(value=bias, name='bias')
-    mu = C.Constant(value=mean, name='aggreagate_mean')
+    mu = C.Constant(value=mean, name='aggregate_mean')
     sigma = C.Constant(value=variance, name='aggregate_variance')
 
     @C.BlockFunction('BatchNormalization', name)
